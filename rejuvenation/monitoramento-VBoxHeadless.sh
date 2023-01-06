723aa9e641849243655d6cb786e3bd61c42dd6e3
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-01-06 15:45:12 -0300

    Final adjustments

--- rejuvenation/monitoramento-VBoxHeadless.sh
+++ rejuvenation/monitoramento-VBoxHeadless.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "cpu mem vmrss vsz threads swap" >monitoramento-VBoxHeadless.txt
-
 pid=$(pidof VBoxHeadless)
 #echo $pid
 if [ -n "$pid" ]; then
   dados=$(pidstat -u -h -p $pid -T ALL -r 1 1 | sed -n '4p')
   thread=$(cat /proc/"$pid"/status | grep Threads | awk '{print $2}')
   cpu=$(echo "$dados" | awk '{print $8}')
-  mem=$(echo "$dados" | awk '{print $14}' | sed 's/,/./g')
+  mem=$(echo "$dados" | awk '{print $14}')
   vmrss=$(echo "$dados" | awk '{print $13}')
   vsz=$(echo "$dados" | awk '{print $12}')
   swap=$(cat /proc/"$pid"/status | grep Swap | awk '{print $2}')
 
-  memtotal=$(awk "BEGIN{ print $mem + $memtotal }" | sed 's/,/./g')
+  vm_total_rss=$(($vmrss + $vm_total_rss))
+  vm_total_vsz=$(($vsz + $vm_total_vsz))
 
   echo "$cpu $mem $vmrss $vsz $thread $swap" >>monitoramento-VBoxHeadless.txt
 else
   echo "pid is empty"
+  exit 1
 fi

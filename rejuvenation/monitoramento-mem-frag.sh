e9b317ef31b603829352e84e89771850342f306b
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-02-03 11:29:41 -0300

    Added memory fragmentation scripts

--- rejuvenation/monitoramento-mem-frag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-result=$(stap memFrag.stp)
-
-#print the first and second separetly row of the result to the file
-used_pages=$(echo "$result" | head -n 1)
-total_pages=$(echo "$result" | tail -n 1)
-echo "$used_pages;$total_pages;$date_time" >>logs/monitoramento-memFrag.txt

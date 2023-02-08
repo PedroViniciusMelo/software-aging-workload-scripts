9d401cb8fdfde917259406ecf7fb92d5ff4fcc36
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-02-08 16:21:53 -0300

    Added script for create VirtualBox disks

---
+++ rejuvenation/setup/create-disks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Universidade Federal Rural de Pernambuco - Unidade Acadêmica de Garanhuns 
+# Uname Research Group
+# Felipe Oliveira 09/11/2018
+
+# Parameters 
+# $1 = number of disks
+# $2 = size (MB)
+
+count=0
+
+while [ $count -lt $1 ]
+do
+    vboxmanage createhd --filename disk$count.vhd --size $2
+    count=`expr $cont + 1`
+done

b20ae38f56c2058d2104c344bd434bb11a76df41
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-02-24 10:10:16 -0300

    Treated curl request error

--- rejuvenation/server-response-time.sh
+++ rejuvenation/server-response-time.sh
@@ -7,18 +7,24 @@
 URL=$1
 
 # Cria o cabeçalho do arquivo CSV
-echo "timestamp,response_time" > response_times.csv
+echo "date_time;response_time" > response_times.csv
 
 # Loop infinito para medir o tempo de resposta
 while true; do
   # Captura o timestamp atual
-  timestamp=$(date +"%Y-%m-%d %T")
+  timestamp=$(date +%d-%m-%Y-%H:%M:%S)
 
   # Faz a requisição HTTP e captura o tempo de resposta
-  response_time=$(curl -w '%{time_total}' -o /dev/null -s "$URL")
+  response=$(curl -w "%{http_code}  %{time_total}" -o /dev/null -s "$URL")
+  code=$(echo $response | awk '{print $1}')
+  response_time=$(echo $response | awk '{print $2}')
+
+  if [ ! "$code" -eq 200 ]; then
+    response_time="-1"
+  fi
 
   # Adiciona o timestamp e o tempo de resposta ao arquivo CSV
-  echo "$timestamp,$response_time" >> response_times.csv
+  echo "$timestamp;$response_time" >> response_times.txt
 
   # Espera 5 segundos antes de fazer a próxima requisição
   sleep 5

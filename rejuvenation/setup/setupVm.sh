5b3b893b9eaec12dfbe25bfd42b10d31ea416499
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-03-28 20:36:26 -0300

    Update setupVm.sh

--- rejuvenation/setup/setupVm.sh
+++ rejuvenation/setup/setupVm.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-vboxmanage import $1
-vboxmanage startvm $1 --type headless
+VBoxManage unregistervm vmDebian --delete
+./remove-disks.sh
+
+mkdir ../disks
+cp create-disks.sh ../disks
+cd ../disks || exit
+
+./create-disks.sh 50 1024
+
+vboxmanage import "$1".ova
+vboxmanage list hdds
+vboxmanage list vms
 
 HOST_IP=$(hostname -I | awk '{print $1}')
-VBoxManage modifyvm $1 --natpf1 "porta 8080,tcp,$HOST_IP,8080,,80"
+VBoxManage modifyvm "$1" --natpf1 "porta 8080,tcp,$HOST_IP,8080,,80"
+
+vboxmanage startvm $1 --type headless
+
+
+
+
 

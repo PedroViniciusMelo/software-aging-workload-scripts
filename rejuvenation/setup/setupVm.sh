faf9f4902722fbf667357f0a51c474046e334161
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-03-30 15:49:06 -0300

    .

--- rejuvenation/setup/setupVm.sh
+++ rejuvenation/setup/setupVm.sh
@@ -10,16 +10,16 @@ cd ../disks || exit
 ./create-disks.sh 51 1024
 
 cd ..
+cd ..
+
 vboxmanage import vmDebian.ova
-vboxmanage list hdds
-vboxmanage list vms
 
 HOST_IP=$(hostname -I | awk '{print $1}')
 VBoxManage modifyvm vmDebian --natpf1 "porta 8080,tcp,$HOST_IP,8080,,80"
 
 vboxmanage startvm vmDebian --type headless
 
-sleep 10
+sleep 20
 
 curl http://localhost:8080
 

27f8ea4a8fdfaecd6ab4098832f2908e7233b192
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2023-05-22 11:29:01 -0300

    Update installvb.sh

--- rejuvenation/setup/installvb.sh
+++ rejuvenation/setup/installvb.sh
@@ -1,4 +1,4 @@
-apt-get install gnupg wget curl -y
+apt-get install gnupg wget curl sysstat -y
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" >> /etc/apt/sources.list
 wget -O oracle_vbox_2016.asc https://www.virtualbox.org/download/oracle_vbox_2016.asc
 gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg oracle_vbox_2016.asc
